9fe04ce9d9ef32da103c0c08a002e851778a89e4
gahgsp <kelvinbianchi@gmail.com>
2025-04-28 18:15:30 +0200

    feat: removed unused parameters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,7 @@ module ActiveSupport
     fixtures :all
 
     # Add more helper methods to be used by all tests here...
-    def sign_in(user, locale: I18n.default_locale)
+    def sign_in(user)
       post session_url, params: { email_address: user.email_address, password: "password" }
     end
   end
